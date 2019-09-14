3ff3eb3a49f29810cde24f02d73f3764d876c990
“tamlh” <kokoro.lehoang@gmail.com>
2019-09-14 09:46:25 +0700

    Add reactstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap'
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
         >
           Learn React
         </a>
+
+        <Button> Hello button </Button>
       </header>
     </div>
   );
